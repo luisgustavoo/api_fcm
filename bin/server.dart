0925f3a9f40fa65493b80032f99ffdacd924de7a
luisgustavoo <luisgustavovierasantos@gmail.com>
2022-12-10 01:23:11 -0300

    Criando POC para enviar push notification v1 via API.

--- bin/server.dart
+++ bin/server.dart
@@ -4,21 +4,8 @@ import 'dart:io';
 import 'package:api_fcm/api_fcm_routers.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
 
 // Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
-}
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
